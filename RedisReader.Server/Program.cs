21124065ed1b90bbf4e8a4a92d867d86d896e025
Andrzej Dajnowski <andrzej.dajnowski@rikstv.no>
2021-02-28 11:56:30 +0100

    removing client project, adding electron lib

--- RedisReader.Server/Program.cs
+++ RedisReader.Server/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElectronNET.API;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +19,10 @@ namespace RedisReader.Server
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseElectron(args);
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
