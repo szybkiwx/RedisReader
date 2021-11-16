1ed73973a4e8a74ec50bdd8403560761b7b3209d
Andrzej Dajnowski <andrzej.dajnowski@rikstv.no>
2021-11-16 12:19:51 +0100

    net6

--- RedisReader.Server/Program.cs
+++ RedisReader.Server/Program.cs
@@ -1,21 +1,11 @@
-using ElectronNET.API;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using RedisReader.Server;
 
-namespace RedisReader.Server
-{
-    public class Program
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+        webBuilder.UseStartup<Startup>();
+    })
+    .Build()
+    .Run();
