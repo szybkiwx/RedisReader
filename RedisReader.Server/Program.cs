e7d41c86ce00247d0ef1013fba2a98a7c92af68f
Andrzej Dajnowski <andrzej.dajnowski@rikstv.no>
2021-11-16 12:26:28 +0100

    preparing for presentation

--- RedisReader.Server/Program.cs
+++ RedisReader.Server/Program.cs
@@ -1,33 +1,24 @@
 using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Blazored.Modal;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using RedisReader.Server;
 using RedisReader.Server.Services;
 
-namespace RedisReader.Server
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<IStoreConnections, ConnectionStore>();
-            builder.Services.AddSingleton<IStoreConnections, ConnectionStore>();
-            builder.Services.AddSingleton<IReadFromRedis, DataReader>();
-            builder.Services.AddSingleton<IManageRedisConnections, ManageRedisConnections>();
-            builder.Services.AddSingleton<ConnectionStateContainer>();
-            builder.Services.AddSingleton<KeyStateContainer>();
-            
-            builder.Services.AddBlazoredModal();
-            builder.Services.AddBlazorContextMenu();
-            
-            
-            await builder.Build().RunAsync();
-        }
-    }
-}
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IStoreConnections, ConnectionStore>();
+builder.Services.AddSingleton<IStoreConnections, ConnectionStore>();
+builder.Services.AddSingleton<IReadFromRedis, DataReader>();
+builder.Services.AddSingleton<IManageRedisConnections, ManageRedisConnections>();
+builder.Services.AddSingleton<ConnectionStateContainer>();
+builder.Services.AddSingleton<KeyStateContainer>();
+
+builder.Services.AddBlazoredModal();
+builder.Services.AddBlazorContextMenu();
+
+
+await builder.Build().RunAsync();
